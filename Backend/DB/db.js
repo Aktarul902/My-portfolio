b63b192de806e9b57f465ca1f7fd6b8dfb698693
unknown <aktarulmondal2740@gmail.com>
2022-03-29 11:03:12 -0700

    last commit

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 function db(){
 
     const dburl = "mongodb://localhost:27017/portfolio"
-    mongoose.connect("`mongodb+srv://aktarul:123aktar@cluster0.gplk5.mongodb.net/portfolio?retryWrites=true&w=majority",{
+    mongoose.connect("``mongodb+srv://aktarul:123aktar@cluster0.gplk5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
         //  useCertificateVerification:false,
         useNewUrlParser:true,
         useNewUrlParser: true,
